09b623d6f7d8df0af1fc8094303ab16014975f92
dwlee0802 <dwlee0802@gmail.com>
2026-02-19 05:36:53 +0000

    Deploying to gh-pages from @ dwlee0802/dwlee0802.github.io@4f1f21856b24fe6eadaca88419f4e4cfe37660de 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,19 +23,19 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "dropdown-blog",
+        },{id: "dropdown-blog",
               title: "blog",
               description: "",
               section: "Dropdown",
               handler: () => {
                 window.location.href = "/blog/";
+              },
+            },{id: "dropdown-bookshelf",
+              title: "bookshelf",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/books/";
               },
             },{id: "post-a-post-with-plotly-js",
         
